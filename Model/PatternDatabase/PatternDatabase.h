9aad205d35c1d3f50d960294345b8720fba1fe00
Salman Hussain <salmanhussain9057@gmail.com>
2023-08-30 07:21:23 +0530

    first commit

---
+++ Model/PatternDatabase/PatternDatabase.h
@@ -0,0 +1,19 @@
+//
+// Created by Salman Hussain on 27-08-2023.
+//
+
+#ifndef RUBIKS_CUBE_SOLVER_PATTERNDATABASE_H
+#define RUBIKS_CUBE_SOLVER_PATTERNDATABASE_H
+
+template<typename T>
+class PatternDatabaseEstimate {
+private:
+
+public:
+    int getEstimate(T rubiksCube) {
+        return 0;
+    }
+
+};
+
+#endif //RUBIKS_CUBE_SOLVER_PATTERNDATABASE_H
